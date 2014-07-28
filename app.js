431ab5e869dc6ed331351168f3e4e22a086a975b
lmaisour <lmaisour@gmail.com>
2014-07-28 09:01:11 -0700

    fucking firebase isnt working

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ ticTacToeApp.controller('appControler', function ($scope) {
 		];
 
 
-		// define winning combinations // 
-		$scope.winners = [[0,1,2],[3,4,5],[6,7,8],[0,3,6], [1,4,7],[2,5,8],[0,4,8],[2,4,6]],
+		// define winning combinations after refactoring // 
+		$scope.winners = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
 
 
 		// just a test to see if the index file is reaching the js file //
